ca568e1c30bbcb04048c7761561285739af787b6
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-05 02:48:00 -0600

    scroll option Tabs MUI

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,30 +4,19 @@ import CardImage from './CardImage';
 
 const CarouselCards = ({ data }) => {
 	return (
-		<>
-			<Tabs
-				className='py-10'
-				variant='scrollable'
-				scrollButtons
-				allowScrollButtonsMobile
-				aria-label='scrollable force tabs example'
-			>
-				{Object.values(data).map((poster, index) => {
-					return (
-						<CardImage data={baseUrlImages + poster} key={index}></CardImage>
-					);
-				})}
-			</Tabs>
-			<div className='w-full flex space-x-5 overflow-x-scroll p-10 pt-48 snap-x snap-mandatory'>
-				{/* <Grid container className=''> */}
-				{Object.values(data).map((poster, index) => {
-					return (
-						<CardImage data={baseUrlImages + poster} key={index}></CardImage>
-					);
-				})}
-				{/* </Grid> */}
-			</div>
-		</>
+		<Tabs
+			className='py-10'
+			variant='scrollable'
+			scrollButtons
+			allowScrollButtonsMobile
+			aria-label='scrollable force tabs example'
+		>
+			{Object.values(data).map((poster, index) => {
+				return (
+					<CardImage data={baseUrlImages + poster} key={index}></CardImage>
+				);
+			})}
+		</Tabs>
 	);
 };
 
