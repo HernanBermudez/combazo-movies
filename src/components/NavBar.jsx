e624350e0259d3c5965f5adb99ea05b35d02c58a
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-08 02:11:09 -0600

    setting up navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,86 +1,178 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import { Card, CardMedia, ListItemButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { Notifications } from '@mui/icons-material';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import SearchBar from './SearchBar';
 
-const TabPanel = props => {
-	const { children, value, index, ...other } = props;
+const pages = [
+	'Inicio',
+	'Series',
+	'Peliculas',
+	'Novedades populares',
+	'Mi lista',
+	'Explora por idiomas',
+];
+const settings = [
+	'Administrar perfil',
+	'Transferir perfil',
+	'Cuenta',
+	'Centro de ayuda',
+	'Cerrar sesión en Netflix',
+];
 
-	return (
-		<div
-			role='tabpanel'
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			{...other}
-		>
-			{value === index && (
-				<Box sx={{ p: 3 }}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
-};
+const ResponsiveAppBar = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-TabPanel.propTypes = {
-	children: PropTypes.node,
-	index: PropTypes.number.isRequired,
-	value: PropTypes.number.isRequired,
-};
-
-const a11yProps = index => {
-	return {
-		id: `simple-tab-${index}`,
-		'aria-controls': `simple-tabpanel-${index}`,
+	const handleOpenNavMenu = event => {
+		setAnchorElNav(event.currentTarget);
 	};
-};
-
-const BasicTabs = () => {
-	const [value, setValue] = React.useState(0);
-
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
+	const handleOpenUserMenu = event => {
+		setAnchorElUser(event.currentTarget);
 	};
 
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Box sx={{ borderBottom: 1, borderColor: 'divider' }} className='pl-8'>
-				<Tabs
-					variant='fullWidth'
-					textColor='white'
-					value={value}
-					onChange={handleChange}
-					aria-label='basic tabs example'
-				>
-					<img src='src/assets/Netflix.png' alt='' width={115}></img>
-					<Tab label='Inicio' {...a11yProps(0)} />
-					<Tab label='Series' {...a11yProps(1)} />
-					<Tab label='Peliculas' {...a11yProps(2)} />
-					<Tab label='Novedades populares' {...a11yProps(1)} />
-					<Tab label='Mi lista' {...a11yProps(2)} />
-					<Tab label='Explora por idiomas' {...a11yProps(1)} />
-					<Tab sx={{ paddingLeft: 50 }} icon={<SearchIcon />} />
-					<Tab icon={<Notifications />} />
-				</Tabs>
-			</Box>
-			<TabPanel value={value} index={0}>
-				Item One
-			</TabPanel>
-			<TabPanel value={value} index={1}>
-				Item Two
-			</TabPanel>
-			<TabPanel value={value} index={2}>
-				Item Three
-			</TabPanel>
-		</Box>
+		<AppBar position='sticky' sx={{ backgroundColor: 'inherit' }}>
+			<Container maxWidth='xl'>
+				<Toolbar disableGutters>
+					<Typography
+						noWrap
+						component='a'
+						href='/'
+						sx={{
+							mr: 2,
+							display: { display: 'flex', paddingLeft: 7 },
+						}}
+					>
+						<img
+							src='src/assets/Netflix.png'
+							alt='Netflix Logo'
+							width={115}
+						></img>
+					</Typography>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: 'flex', md: 'none' },
+						}}
+					>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='inherit'
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: 'block', md: 'none' },
+							}}
+						>
+							{pages.map(page => (
+								<MenuItem key={page} onClick={handleCloseNavMenu}>
+									<Typography textAlign='center'>{page}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+					<Typography
+						variant='h5'
+						noWrap
+						component='a'
+						href=''
+						sx={{
+							mr: 2,
+							display: { xs: 'flex', md: 'none' },
+							flexGrow: 1,
+							fontFamily: 'monospace',
+							fontWeight: 700,
+							letterSpacing: '.3rem',
+							color: 'inherit',
+							textDecoration: 'none',
+						}}
+					>
+						LOGO
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+						{pages.map(page => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: 'white', display: 'block' }}
+							>
+								{page}
+							</Button>
+						))}
+					</Box>
+					<SearchBar />
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title='Open settings'>
+							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: '45px' }}
+							id='menu-appbar'
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							{settings.map(setting => (
+								<MenuItem key={setting} onClick={handleCloseUserMenu}>
+									<Typography textAlign='center'>{setting}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 };
-
-export default BasicTabs;
+export default ResponsiveAppBar;
 
