a2761fe839a7dc68676be477b8b85212b4845c3c
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-08 02:11:20 -0600

    setting up searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,62 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
-export default function SearchBar() {
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	},
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
+			},
+		},
+	},
+}));
+
+const SearchBar = () => {
 	return (
-		<Box
-			component='form'
-			sx={{
-				'& > :not(style)': { m: 1, width: '25ch' },
-			}}
-			noValidate
-			autoComplete='off'
-		>
-			<TextField id='outlined-basic' label='Outlined' variant='outlined' />
-			<TextField id='filled-basic' label='Filled' variant='filled' />
-			<TextField id='standard-basic' label='Standard' variant='standard' />
-		</Box>
+		<Search>
+			<SearchIconWrapper>
+				<SearchIcon />
+			</SearchIconWrapper>
+			<StyledInputBase
+				placeholder='Search…'
+				inputProps={{ 'aria-label': 'search' }}
+			/>
+		</Search>
 	);
-}
+};
+
+export default SearchBar;
 
