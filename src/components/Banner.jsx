cd52784364ee9ea2ac0c68ccca13ccec7fc3934a
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-10 03:19:36 -0600

    Creation of banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import { Card, CardMedia } from '@mui/material';
+import React from 'react';
+
+const Banner = () => {
+	return (
+		<Card sx={{ minWidth: 'full', position: 'static' }}>
+			<CardMedia
+				component='iframe'
+				src='https://www.youtube.com/embed/d9MyW72ELq0?autoplay=1&controls=0&modestbranding=1&showinfo=0&end=60&rel=0'
+				title='Wonderwoman Trailer'
+				height={600}
+			/>
+		</Card>
+	);
+};
+
+export default Banner;
+
