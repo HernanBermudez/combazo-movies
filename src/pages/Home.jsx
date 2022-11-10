27f19c6eb573fc67222b5e25ad7571ce370232f0
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-10 03:19:22 -0600

    Setting banner in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from '../components/Banner';
 import Carousel from '../components/Carousel';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
@@ -7,11 +8,14 @@ const Home = () => {
 	const { trendingMovies, trendingSeries, trendingAll } = useMovies();
 	return (
 		<>
+			<Banner />
 			<NavBar />
 			<Carousel data={trendingAll} />
 			<Carousel data={trendingSeries} />
 			<Carousel data={trendingMovies} />
-
+			<Carousel data={trendingAll} />
+			<Carousel data={trendingSeries} />
+			<Carousel data={trendingMovies} />
 			<Footer />
 		</>
 	);
