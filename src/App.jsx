84e0c203ca1158f29a0767c2fdcb6add26c60d48
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-18 01:49:43 -0600

    added new route

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const App = () => {
 					<Routes>
 						<Route path='/' element={<Layout />}>
 							<Route index element={<Home />} />
+							<Route path='/login' element={<Login />} />
 							<Route path='/series' element={<Series />} />
 							<Route path='/movies' element={<Movies />} />
 							<Route path='/latest' element={<Latest />} />
