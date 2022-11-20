9661786155f3f19a2972ee765fe5fbd933e69f23
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-20 03:10:39 -0600

    two types of carousel, search and normal

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,34 +2,41 @@ import { Grid, Tabs } from '@mui/material';
 import TMDB from '../config/TMDB';
 import CardImage from './CardImage';
 
-const Carousel = ({ data }) => {
-	const posters = Object.values(data).map(info => info.backdrop_path);
+const Carousel = ({ data, search }) => {
+	const posters = Object.values(data).map(info => info.poster_path);
 	posters.pop();
 	const title = data.type;
 	return (
 		<>
 			<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
-			{/* <Grid
-				container
-				className='w-full flex space-x-5 overflow-x-scroll p-10 pt-48 snap-x snap-mandatory'
-			> */}
-			<Tabs
-				value={0}
-				variant='scrollable'
-				scrollButtons
-				allowScrollButtonsMobile
-				//aria-label='scrollable force tabs example'
-			>
-				{Object.values(posters).map((poster, index) => {
-					return (
-						<CardImage
-							data={TMDB.images.base_url + TMDB.images.backdrop.w300 + poster}
-							key={index}
-						></CardImage>
-					);
-				})}
-			</Tabs>
-			{/* </Grid> */}
+			{search ? (
+				<Grid container className='flex pl-20 space-y-16 '>
+					{Object.values(posters).map((poster, index) => {
+						return (
+							<CardImage
+								data={TMDB.images.base_url + TMDB.images.backdrop.w300 + poster}
+								key={index}
+							></CardImage>
+						);
+					})}
+				</Grid>
+			) : (
+				<Tabs
+					value={0}
+					variant='scrollable'
+					scrollButtons
+					allowScrollButtonsMobile
+				>
+					{Object.values(posters).map((poster, index) => {
+						return (
+							<CardImage
+								data={TMDB.images.base_url + TMDB.images.backdrop.w300 + poster}
+								key={index}
+							></CardImage>
+						);
+					})}
+				</Tabs>
+			)}
 		</>
 	);
 };
