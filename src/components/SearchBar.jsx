897568149d163aff8097c1b3179698b09bc11bb8
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-25 04:12:56 -0600

    search done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,17 +3,20 @@ import useMovies from '../hooks/useMovies';
 import searchContent from '../services/searchContent';
 import { useNavigate } from 'react-router-dom';
 import stylesSearchBar from '../helpers/stylesSearchBar';
+import { routes } from '../helpers/constants';
 
 const SearchBar = () => {
 	const { StyledInputBase, SearchIconWrapper, Search } = stylesSearchBar();
 	const { setSearch } = useMovies();
 	const navigate = useNavigate();
+
 	const handleSearch = searchRequest => {
+		searchRequest.preventDefault();
 		setTimeout(() => {
 			searchContent(searchRequest.target.value)
 				.then(data => setSearch(data))
 				.catch(error => console.log(error));
-			navigate(`/search/${searchRequest.target.value}`);
+			navigate(`${routes[6]} + ${searchRequest.target.value}`);
 		}, 3000);
 
 		return searchRequest;
