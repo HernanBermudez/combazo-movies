803b9b6d2e44892f0455399cb86e1e0e1ecc8b18
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-25 03:02:05 -0600

    optimized banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,17 @@
 import { Card, CardMedia } from '@mui/material';
 const Banner = ({ data }) => {
 	return (
-		<Card sx={{ position: 'static', minWidth: 'full' }}>
-			<CardMedia
-				component='iframe'
-				src={data[Math.floor(Math.random() * data.length)]}
-				title='Trailer'
-				height={600}
-			/>
-		</Card>
+		data && (
+			<Card sx={{ position: 'static', minWidth: 'full' }}>
+				<CardMedia
+					component='iframe'
+					src={data[Math.floor(Math.random() * data.length)]}
+					title='Trailer'
+					height={600}
+					loading={'lazy'}
+				/>
+			</Card>
+		)
 	);
 };
 
