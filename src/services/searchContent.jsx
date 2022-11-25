bfbcd10e79042429058f22acb84cb756d8f42f18
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-24 23:05:53 -0600

    applied deep destructuring

--- src/services/searchContent.jsx
+++ src/services/searchContent.jsx
@@ -2,9 +2,11 @@ import TMDB from '../config/TMDB';
 
 const searchContent = async search => {
 	try {
-		const { data } = await TMDB.api.get(TMDB.paths.search + '?query=' + search);
-		//console.log(data.results);
-		return data.results;
+		const {
+			data: { results },
+		} = await TMDB.api.get(TMDB.paths.search + '?query=' + search);
+		results.filter(key => key.poster_path !== undefined);
+		return results;
 	} catch (error) {
 		return console.log(error);
 	}
