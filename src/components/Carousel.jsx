677ce2424577b91a42c8d637cecc02a5e459a51a
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-25 03:01:49 -0600

    visual bugs fixes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,18 +3,18 @@ import TMDB from '../config/TMDB';
 import CardImage from './CardImage';
 
 const Carousel = ({ data, search }) => {
-	const posters = Object.values(data).map(info => info.poster_path);
+	const posters = Object.values(data).map(key => key.poster_path);
 	posters.pop();
 	const title = data.type;
 	return (
 		<>
 			<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
 			{search ? (
-				<Grid container className='flex pl-20 space-y-16 '>
+				<Grid container className='flex pl-20 gap-y-10  '>
 					{Object.values(posters).map((poster, index) => {
 						return (
 							<CardImage
-								data={TMDB.images.base_url + TMDB.images.backdrop.w300 + poster}
+								data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
 								key={index}
 							></CardImage>
 						);
@@ -30,7 +30,7 @@ const Carousel = ({ data, search }) => {
 					{Object.values(posters).map((poster, index) => {
 						return (
 							<CardImage
-								data={TMDB.images.base_url + TMDB.images.backdrop.w300 + poster}
+								data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
 								key={index}
 							></CardImage>
 						);
