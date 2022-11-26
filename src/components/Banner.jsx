7bf9ca00a644f6368ce4230a5317918671ce7574
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-26 04:05:17 -0600

    conditional rendering

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,39 @@
 import { Card, CardMedia } from '@mui/material';
-const Banner = ({ data }) => {
+import { useState } from 'react';
+import getRandomContent from '../helpers/getRandomContent';
+import getBackdrops from '../services/getBackdrops';
+
+const Banner = ({ data, trailers }) => {
+	const backdrops = getBackdrops(data);
+	const [isLoading, setLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const randomContent = getRandomContent(data);
+
+	// const timer = setTimeout(() => {
+	// 	setLoading(false);
+	// 	return clearTimeout(timer);
+	// }, 5000);
+
+	// if (isLoading) {
+	// 	return (
+	// 		trailers && (
+	// 			<Card sx={{ position: 'static', minWidth: 'full' }}>
+	// 				<CardMedia component='img' alt='' image={backdrops[randomContent]} />
+	// 			</Card>
+	// 		)
+	// 	);
+	// }
 	return (
-		data && (
+		trailers && (
 			<Card sx={{ position: 'static', minWidth: 'full' }}>
 				<CardMedia
 					component='iframe'
-					src={data[Math.floor(Math.random() * data.length)]}
+					src={trailers[randomContent]}
 					title='Trailer'
 					height={600}
 					loading={'lazy'}
 				/>
+				<CardMedia component='img' alt='' image={backdrops[randomContent]} />
 			</Card>
 		)
 	);
