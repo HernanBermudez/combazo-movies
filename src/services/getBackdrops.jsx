26e091f018776363012bc42aca74d6483ca6e8c2
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-26 01:36:20 -0600

    resolved

--- src/services/getBackdrops.jsx
+++ src/services/getBackdrops.jsx
@@ -1,8 +1,11 @@
-const getBackdrops = content => {
-	const backdrops = Object.values(content).map(key => key.backdrop_path);
-	backdrops.pop();
-	console.log(backdrops);
+import TMDB from '../config/TMDB';
 
+const getBackdrops = content => {
+	const backdropsPath = Object.values(content).map(key => key.backdrop_path);
+	backdropsPath.pop();
+	const backdrops = backdropsPath.map(
+		backdrop => TMDB.images.base_url + TMDB.images.sizes.original + backdrop
+	);
 	return backdrops;
 };
 
