532a88c2bd457a657725a7a1036d25559627cd37
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-27 23:56:37 -0600

    media type in data fetched

--- src/helpers/getContentId.jsx
+++ src/helpers/getContentId.jsx
@@ -1,7 +1,7 @@
 const getContentId = content => {
-	const type = content.type;
 	const ids = Object.values(content).map(id => id.id);
 	ids.pop();
+	const type = Object.values(content).find(key => key.media_type);
 	return { ids, type };
 };
 
