5bc4cbbf87c3bebdd5c95b4f18b49fbb1fffc238
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-28 02:06:46 -0600

    clean

--- src/helpers/getContentId.jsx
+++ src/helpers/getContentId.jsx
@@ -2,8 +2,7 @@ const getContentId = content => {
 	const ids = Object.values(content).map(id => id.id);
 	ids.pop();
 	const { media_type } = Object.values(content).find(key => key.media_type);
-	console.log(media_type);
-	return ids, media_type;
+	return { ids, media_type };
 };
 
 export default getContentId;
