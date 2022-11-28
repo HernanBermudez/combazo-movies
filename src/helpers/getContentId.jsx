56146f58d50cda7285e56fc6ff6f8b9dbbfe32a6
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-28 01:29:44 -0600

    media type destructured from content

--- src/helpers/getContentId.jsx
+++ src/helpers/getContentId.jsx
@@ -1,8 +1,9 @@
 const getContentId = content => {
 	const ids = Object.values(content).map(id => id.id);
 	ids.pop();
-	const type = Object.values(content).find(key => key.media_type);
-	return { ids, type };
+	const { media_type } = Object.values(content).find(key => key.media_type);
+	console.log(media_type);
+	return ids, media_type;
 };
 
 export default getContentId;
