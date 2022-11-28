107e4cd854b4fd1cd0b63f76a6e78cb01db61e78
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-28 01:54:02 -0600

    finally done, trailers filtered

--- src/services/getTrailers.jsx
+++ src/services/getTrailers.jsx
@@ -5,7 +5,7 @@ const getTrailers = async data => {
 	const ids = Object.values(data).map(id => id.id);
 	ids.pop();
 	const { media_type } = Object.values(data).find(key => key.media_type);
-	console.log(media_type);
+
 	const trailersInfo = await Promise.all(
 		ids.map(
 			async id =>
@@ -15,6 +15,7 @@ const getTrailers = async data => {
 					.catch(error => console.log(error))
 		)
 	);
+
 	const trailersKey = trailersInfo.flatMap(key => {
 		const trailersObject = key.map(
 			objects => objects.type == 'Trailer' && objects.key
@@ -22,14 +23,8 @@ const getTrailers = async data => {
 		return trailersObject;
 	});
 
-	// .then(response => response.data.results)
-	// .then(results => results.map(key => key.key))
-	// .then(keys =>
-	// 	keys.map((key, index) => {
-	// 		trailerUrl[0] + key[index] + trailerUrl[1];
-	// 	})
-	// )
-	console.log(trailersKey);
+	const trailersFiltered = trailersKey.filter(key => key !== false);
+	console.log(trailersFiltered);
 	return trailersInfo;
 };
 
