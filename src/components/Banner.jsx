0cbf9e1722bf57966c86e525963cde81987987df
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-11-30 23:54:46 -0600

    amazing transition backdrops to trailer RIO

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,46 +1,45 @@
 import { Card, CardMedia } from '@mui/material';
-import { useRef } from 'react';
+import { useEffect } from 'react';
 import { trailerUrl } from '../helpers/constants';
-import useIsInViewport from '../hooks/useIsInViewport';
 import getBackdrops from '../services/getBackdrops';
 import useMovies from '../hooks/useMovies';
+import { useInView } from 'react-intersection-observer';
 const Banner = ({ data, trailers }) => {
-	const { transition, setTransition, random } = useMovies();
-	const ref1 = useRef(null);
-
-	const isInViewport1 = useIsInViewport(ref1);
-	console.log('isInViewport1: ', isInViewport1);
-
-	const handleTransition = () => {
-		setTimeout(() => {
-			setTransition(true);
-		}, 4000);
-		return setTransition(false);
-	};
+	const { random, transition, setTransition } = useMovies();
+	const [ref, inView] = useInView({
+		triggerOnce: false,
+		marginRoot: '0px 0px',
+	});
 	const backdrops = getBackdrops(data);
 
+	useEffect(() => {
+		const handleTransition = setTimeout(() => {
+			setTransition(false);
+		}, 6000);
+		return () => {
+			setTransition(true);
+			clearTimeout(handleTransition);
+		};
+	}, [inView]);
+
 	return (
-		<Card ref={ref1} sx={{ position: 'static', minWidth: 'full' }}>
-			{isInViewport1 && (
-				<>
-					{transition === false ? (
-						<CardMedia
-							component='img'
-							alt=''
-							image={backdrops[random]}
-							onMouseMove={handleTransition}
-						/>
-					) : (
-						<CardMedia
-							component='iframe'
-							src={trailerUrl[0] + trailers[random] + trailerUrl[1]}
-							title='Trailer'
-							height={600}
-							loading={'lazy'}
-						/>
-					)}
-				</>
-			)}
+		<Card
+			ref={ref}
+			sx={{ position: 'static', minWidth: 'full', maxHeight: 600 }}
+		>
+			<>
+				{transition === true ? (
+					<CardMedia component='img' alt='' image={backdrops[random]} />
+				) : (
+					<CardMedia
+						component='iframe'
+						src={trailerUrl[0] + trailers[random] + trailerUrl[1]}
+						title='Trailer'
+						height={600}
+						loading={'lazy'}
+					/>
+				)}
+			</>
 		</Card>
 	);
 };
