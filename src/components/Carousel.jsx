e6f8ebeb8617c2d4534439161d4d1681a1de6999
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-12-05 00:26:00 -0600

    offset y applied

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,38 +3,62 @@ import TMDB from '../config/TMDB';
 import getPosters from '../services/getPosters';
 import CardImage from './CardImage';
 
-const Carousel = ({ data, search }) => {
+const Carousel = ({ data, search, latest }) => {
 	const { posters, title } = getPosters(data);
 	return (
 		<>
-			<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
 			{search ? (
-				<Grid container className='flex pl-20 gap-y-10  '>
-					{Object.values(posters).map((poster, index) => {
-						return (
-							<CardImage
-								data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
-								key={index}
-							></CardImage>
-						);
-					})}
-				</Grid>
+				<>
+					<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
+					<Grid container className='flex pl-20 gap-y-10  '>
+						{Object.values(posters).map((poster, index) => {
+							return (
+								<CardImage
+									data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
+									key={index}
+								></CardImage>
+							);
+						})}
+					</Grid>
+				</>
+			) : latest ? (
+				<>
+					<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
+					<Tabs
+						value={0}
+						variant='scrollable'
+						scrollButtons
+						allowScrollButtonsMobile
+					>
+						{Object.values(posters).map((poster, index) => {
+							return (
+								<CardImage
+									data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
+									key={index}
+								></CardImage>
+							);
+						})}
+					</Tabs>
+				</>
 			) : (
-				<Tabs
-					value={0}
-					variant='scrollable'
-					scrollButtons
-					allowScrollButtonsMobile
-				>
-					{Object.values(posters).map((poster, index) => {
-						return (
-							<CardImage
-								data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
-								key={index}
-							></CardImage>
-						);
-					})}
-				</Tabs>
+				<div className='-translate-y-36'>
+					<div className='text-xl font-semibold pl-10 pb-2 pt-10'>{title}</div>
+					<Tabs
+						value={0}
+						variant='scrollable'
+						scrollButtons
+						allowScrollButtonsMobile
+					>
+						{Object.values(posters).map((poster, index) => {
+							return (
+								<CardImage
+									data={TMDB.images.base_url + TMDB.images.poster.w300 + poster}
+									key={index}
+								></CardImage>
+							);
+						})}
+					</Tabs>
+				</div>
 			)}
 		</>
 	);
